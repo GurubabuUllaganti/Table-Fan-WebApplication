9dbb8260348ffb487cc37d67a53aa578ec771db7
GurubabuUllaganti <137080329+GurubabuUllaganti@users.noreply.github.com>
2024-02-02 12:56:13 +0530

    Add files via upload

---
+++ FanJs.js
@@ -0,0 +1,21 @@
+let a=document.getElementById('img');
+function fanOn()
+{
+    a.style.animationDuration =3+"s";
+}
+function fanOff()
+{
+    a.style.animationDuration =0+"s";
+}
+function fanSpeed1()
+{
+    a.style.animationDuration =1+"s";
+}
+function fanSpeed2()
+{
+    a.style.animationDuration =0.5+"s";
+}
+function fanSpeed3()
+{
+    a.style.animationDuration =0.1+"s";
+}
